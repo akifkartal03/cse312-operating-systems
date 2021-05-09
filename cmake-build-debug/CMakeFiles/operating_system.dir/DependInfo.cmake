be404a7737752af40e240321693c4cb005f9d0b1
akifkartal03 <akokartal@gmail.com>
2021-05-09 16:39:52 +0300

    add last

---
+++ cmake-build-debug/CMakeFiles/operating_system.dir/DependInfo.cmake
@@ -0,0 +1,87 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/akif/Desktop/operating-system/HW1/shell.c" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW1/shell.c.o"
+  "/home/akif/Desktop/operating-system/HW1/test.c" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW1/test.c.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/xspim/buttons.c" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/xspim/buttons.c.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/xspim/windows.c" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/xspim/windows.c.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/xspim/xspim.c" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/xspim/xspim.c.o"
+  "/home/akif/Desktop/operating-system/Practices/hw1/bubble_sort.c" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/Practices/hw1/bubble_sort.c.o"
+  "/home/akif/Desktop/operating-system/Practices/hw1/factors.c" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/Practices/hw1/factors.c.o"
+  "/home/akif/Desktop/operating-system/Practices/hw1/show_primes.c" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/Practices/hw1/show_primes.c.o"
+  "/home/akif/Desktop/operating-system/Practices/hw1/test.c" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/Practices/hw1/test.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../HW1"
+  "../HW2/spim/CPU"
+  "../HW2/spim/PCSpim"
+  "../HW2/spim/QtSpim"
+  "../HW2/spim/spim"
+  "../HW2/spim/xspim"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/akif/Desktop/operating-system/HW1/syscall.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW1/syscall.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/CPU/data.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/CPU/data.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/CPU/display-utils.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/CPU/display-utils.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/CPU/dump_ops.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/CPU/dump_ops.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/CPU/inst.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/CPU/inst.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/CPU/mem.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/CPU/mem.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/CPU/run.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/CPU/run.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/CPU/spim-utils.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/CPU/spim-utils.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/CPU/string-stream.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/CPU/string-stream.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/CPU/sym-tbl.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/CPU/sym-tbl.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/CPU/syscall.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/CPU/syscall.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/BreakpointDlg.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/BreakpointDlg.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/ConsoleWnd.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/ConsoleWnd.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/MainFrm.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/MainFrm.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/MultiStepDlg.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/MultiStepDlg.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/PCSpim.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/PCSpim.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/PCSpimDoc.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/PCSpimDoc.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/PCSpimView.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/PCSpimView.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/Regman.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/Regman.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/RunDlg.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/RunDlg.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/SetValueDlg.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/SetValueDlg.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/SettingsDlg.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/SettingsDlg.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/SpimSupport.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/SpimSupport.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/StdAfx.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/StdAfx.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/PCSpim/util.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/PCSpim/util.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/QtSpim/console.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/QtSpim/console.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/QtSpim/datawin.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/QtSpim/datawin.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/QtSpim/main.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/QtSpim/main.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/QtSpim/menu.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/QtSpim/menu.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/QtSpim/parser_yacc.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/QtSpim/parser_yacc.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/QtSpim/regwin.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/QtSpim/regwin.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/QtSpim/scanner_lex.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/QtSpim/scanner_lex.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/QtSpim/spim_support.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/QtSpim/spim_support.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/QtSpim/spimview.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/QtSpim/spimview.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/QtSpim/state.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/QtSpim/state.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/QtSpim/textwin.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/QtSpim/textwin.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/spim/lex.yy.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/spim/lex.yy.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/spim/parser_yacc.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/spim/parser_yacc.cpp.o"
+  "/home/akif/Desktop/operating-system/HW2/spim/spim/spim.cpp" "/home/akif/Desktop/operating-system/cmake-build-debug/CMakeFiles/operating_system.dir/HW2/spim/spim/spim.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../HW1"
+  "../HW2/spim/CPU"
+  "../HW2/spim/PCSpim"
+  "../HW2/spim/QtSpim"
+  "../HW2/spim/spim"
+  "../HW2/spim/xspim"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
